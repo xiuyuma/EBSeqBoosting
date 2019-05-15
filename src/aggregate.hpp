8a97ed1ba6048802a4fd8ce7caa505fd9c7c999a
xiuyuma <ma79@wisc.edu>
2019-05-15 16:54:10 -0500

    aggr

--- src/aggregate.hpp
+++ src/aggregate.hpp
@@ -1,13 +1,15 @@
 #pragma once
 
 #include <Eigen/Dense>
+#include <vector>
+#include <algorithm>
 
 struct helper
 {
     static std::vector<int> which(std::vector<int> clus, int i)
     {
-        if(clus.size < 1 || std::find(clus.begin(),clus.end(),i) == clus.end())
-            return std::vector<int>;
+        if(clus.size() < 1 || std::find(clus.begin(),clus.end(),i) == clus.end())
+            return std::vector<int>();
         
         std::vector<int> res;
         
@@ -22,9 +24,23 @@ struct helper
         return res;
     }
     
-    static bool clusCheck(vector<int> clus)
+    // clus should be array of 1 - K
+    static bool clusCheck(std::vector<int> clus)
     {
+        int smallest = *std::min_element(clus.begin(),clus.end());
         
+        if(smallest != 1)
+            return false;
+        
+        int largest = *std::max_element(clus.begin(),clus.end());
+        
+        for(int t = 1; t < largest + 1; t++)
+        {
+            if(std::find(clus.begin(),clus.end(),t) == clus.end())
+                return false;
+        }
+        
+        return true;
     }
 }
 

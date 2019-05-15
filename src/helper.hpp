117ac9f87ef8939624510d5898b28abd5b19a3b8
xiuyuma <ma79@wisc.edu>
2019-05-15 17:39:31 -0500

    organize

--- src/helper.hpp
+++ src/helper.hpp
@@ -4,25 +4,15 @@
 #include <vector>
 #include <algorithm>
 
+
+struct CLUSINFO
+{
+    std::vector<std::vector<int> > index;
+    std::vector<int> size;
+};
+
 struct helper
 {
-    static std::vector<int> which(std::vector<int> clus, int i)
-    {
-        if(clus.size() < 1 || std::find(clus.begin(),clus.end(),i) == clus.end())
-            return std::vector<int>();
-        
-        std::vector<int> res;
-        
-        for(size_t t = 0; t < clus.size(); t++)
-        {
-            if(clus[t] == i)
-            {
-                res.push_back(t);
-            }
-        }
-        
-        return res;
-    }
     
     // clus should be array of 1 - K
     static bool clusCheck(std::vector<int> clus)
@@ -43,10 +33,49 @@ struct helper
         return true;
     }
     
-    static vector<int> clusSize(std::vector<int> clus)
+    static std::vector<int> which(std::vector<int> clus, int i)
     {
+        if(clus.size() < 1 || std::find(clus.begin(),clus.end(),i) == clus.end())
+            return std::vector<int>();
         
+        std::vector<int> res;
+        
+        for(size_t t = 0; t < clus.size(); t++)
+        {
+            if(clus[t] == i)
+            {
+                res.push_back(t);
+            }
+        }
+        
+        return res;
     }
+    
+    
+    static CLUSINFO clusInfo(std::vector<int> clus)
+    {
+        int K = *std::max_element(clus.begin(),clus.end());
+        
+        std::vector<std::vector<int> > index;
+        
+        std::vector<int> size;
+        
+        for(int i = 1; i < K + 1; i++)
+        {
+            index.push_back(which(clus,i));
+            
+            size.push_back(index[i - 1].size());
+        }
+        
+        CLUSINFO res;
+        
+        res.index = index;
+        
+        res.size = size;
+        
+        return res;
+    }
+    
 };
 
 

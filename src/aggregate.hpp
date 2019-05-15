fccb94ab01ac590e24fc835e9fa435c069afd856
xiuyuma <ma79@wisc.edu>
2019-05-15 17:45:53 -0500

    aggregates

--- src/aggregate.hpp
+++ src/aggregate.hpp
@@ -3,9 +3,14 @@
 #include <Eigen/Dense>
 #include <vector>
 #include <algorithm>
-
+#include "helper.hpp"
 
 struct aggregate
 {
+    using COUNTS = Eigen::MatrixXi;
     
+    static COUNTS aggregate(COUNTS& counts, CLUSINFO clusInfo)
+    {
+        
+    }
 };

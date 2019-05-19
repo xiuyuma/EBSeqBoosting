c407a6fffe12bf1db6ad8295a90c7c45aa26f2c6
xiuyuma <ma79@wisc.edu>
2019-05-19 13:06:11 -0500

    namespcae

--- src/aggregate.hpp
+++ src/aggregate.hpp
@@ -4,10 +4,15 @@
 #include <vector>
 #include <algorithm>
 #include "helper.hpp"
+#include "float.hpp"
+#include "counts.hpp"
+
+namespace EBS
+{
+    
 
 struct aggregate
 {
-    using COUNTS = Eigen::MatrixXf;
     
     static COUNTS sum(COUNTS& counts, CLUSINFO& clusInfo)
     {
@@ -29,16 +34,16 @@ struct aggregate
     static COUNTS groupMean(COUNTS& counts, CLUSINFO& clusInfo)
     {
         size_t K = clusInfo.size.size();
-        
+
         COUNTS _sum = sum(counts, clusInfo);
         
         for(auto i = 0; i < K; i++)
         {
             size_t _size = clusInfo.index[i].size();
-            
+
             _sum.col(i) /= _size;
         }
-        
+
         return _sum;
     }
     
@@ -51,22 +56,24 @@ struct aggregate
     static COUNTS groupVar(COUNTS& counts, CLUSINFO& clusInfo)
     {
         size_t K = clusInfo.size.size();
-        
+
         COUNTS _mean = groupMean(counts, clusInfo);
-        
+
         COUNTS res(counts.rows(),K);
-        
+
         for(int i = 0; i < K; i++)
         {
             for(auto s:clusInfo.index[i])
             {
-                res.col(i) += (counts.col(s) - _mean.col(i)).unaryExpr(&square<float>);
+                res.col(i) += (counts.col(s) - _mean.col(i)).unaryExpr(&square<Float>);
             }
-            
+
             res.col(i) /= clusInfo.index[i].size();
         }
-        
+
         return res;
     }
     
+};
+
 };

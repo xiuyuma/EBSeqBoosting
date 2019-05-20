dd75e4f001ea942b3aa6954ec41c286a823c489e
xiuyuma <ma79@wisc.edu>
2019-05-19 19:08:59 -0500

    add skeleton

--- src/negativeBinomial.hpp
+++ src/negativeBinomial.hpp
@@ -8,6 +8,25 @@ namespace EBS
   
     class NB:public EBSeq
     {
+    public:
+        
+        void init(COUNTS& counts, CLUSINFO& clusInfo, std::vector<Float> hyperParam)
+        {
+            _sum = aggregate::sum(counts, clusInfo);
+            
+            _hp = hyperParam;
+        }
+        
+        
+        Float kernel(COUNTS& _sum, std::vector<Float>& hyperParam)
+        {
+            
+        }
+        
+        
+    private:
+        
+        COUNTS _r;
         
     };
     
